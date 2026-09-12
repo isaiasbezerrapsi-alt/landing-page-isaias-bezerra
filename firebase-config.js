201f7342a24d26b651e33e2dea3848fc28318183
isaiasbezerrapsi-alt <isaiasbezerrapsi@gmail.com>
2026-09-12 13:53:32 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,9 @@
-/*
-  CONFIGURAÇÃO DAS AVALIAÇÕES COM GOOGLE + FIREBASE
-  1. Crie um projeto em https://console.firebase.google.com/
-  2. Ative Authentication > Sign-in method > Google.
-  3. Crie um Firestore Database.
-  4. Registre um app Web e copie a configuração abaixo.
-  5. Substitua os valores pelos dados do seu projeto.
-*/
 window.ISAIAS_FIREBASE_CONFIG = {
-  apiKey: "COLOQUE_SUA_API_KEY",
-  authDomain: "SEU-PROJETO.firebaseapp.com",
-  projectId: "SEU-PROJETO",
-  storageBucket: "SEU-PROJETO.firebasestorage.app",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBKLdY34dzXaR0KIamdZ0oIDGaJvPwo4pw",
+  authDomain: "isaiasbezerraavaliacoes.firebaseapp.com",
+  projectId: "isaiasbezerraavaliacoes",
+  storageBucket: "isaiasbezerraavaliacoes.firebasestorage.app",
+  messagingSenderId: "876617217117",
+  appId: "1:876617217117:web:921ce7c23d0e2ee39489b4",
+  measurementId: "G-6HBYSY7Y13"
 };
